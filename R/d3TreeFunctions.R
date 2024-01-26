6dcd87a736cac3e865cf074638c5301c548190f9
Jonathan Sidi <yoni@pinpointstrategies.io>
2024-01-26 09:35:11 -0500

    refactor imports a bit more switch to mit license

--- R/d3TreeFunctions.R
+++ R/d3TreeFunctions.R
@@ -1,50 +1,3 @@
-make_list <- function(x,value=NULL) {
-
-  if(is.null(value)){
-    value <- names(x)
-  }
-
-  idx <- is.na(x[,2])
-  if (ncol(x) > 2 && sum(idx) != nrow(x)){
-
-    listSplit <- split(x[-1], x[1], drop = TRUE)
-    colName <- value[1]
-    value <- value[-1]
-
-    lapply(names(listSplit), function(y){
-      list(
-        name = y, 
-        value = colName, 
-        children = make_list(listSplit[[y]],value=value)
-        )
-      })
-  } else {
-
-    if(!all(is.na(x[,1]))){
-
-      nms <- x[,1]
-      col_name <- value[1]
-
-      lapply(seq_along(nms), function(y){
-        list(
-          name = nms[y],
-          value = col_name
-        )
-      })
-    }
-  }
-}
-
-renquote <- function(l) {
-
-  if (is.list(l)){
-    lapply(l, renquote)
-  } else {
-    enquote(l)
-  }
-
-}
-
 #data.frame to json sent to JS code
 #' @title df2tree
 #'
@@ -56,12 +9,16 @@ renquote <- function(l) {
 #'   nodes in each hierarchy
 #'
 #' @examples
-#' df2tree(struct = as.data.frame(Titanic),rootname = 'Titanic')
+#' titanic_df <- as.data.frame(Titanic)
+#'
+#' df2tree(struct = titanic_df,rootname = 'Titanic')
+#'
 #' df2tree(
-#'   struct = as.data.frame(Titanic),
+#'   struct = titanic_df,
 #'   rootname = 'Titanic',
 #'   tool_tip = letters[1:5]
 #' )
+#'
 #' @export
 df2tree <- function(struct, rootname = 'root', tool_tip = NULL) {
 
@@ -70,7 +27,7 @@ df2tree <- function(struct, rootname = 'root', tool_tip = NULL) {
   }
 
   list(
-    name = rootname, 
+    name = rootname,
     children = make_list(
       struct,
       value = tool_tip[-1]
@@ -87,11 +44,12 @@ df2tree <- function(struct, rootname = 'root', tool_tip = NULL) {
 #' @export
 #' @keywords internal
 #' @importFrom tibble tibble
+#' @importFrom tidyselect everything
 #' @importFrom utils globalVariables head
-#' @import dplyr
+#' @importFrom dplyr mutate select arrange filter across summarise bind_rows
 tree_filter <- function(nodes_list, m){
 
-   nodesdf <- tibble::tibble(
+  nodesdf <- tibble::tibble(
     rowname = names(nodes_list),
     x = nodes_list
   )
@@ -112,14 +70,14 @@ tree_filter <- function(nodes_list, m){
     )
 
     x_count <- 10^-(str_counts - 1)
-    x_count_depth <- c(0,str_counts)
+    x_count_depth <- c(0, str_counts)
     x_depth <- max(x_count_depth)
 
     node_id <- seq(length(x_count_depth))
     parent_id <- rep(0, length(x_count) + 1)
 
     x_temp <- rbind(unique(x_count_depth), rep(0,x_depth + 1))
-    x_temp[2,1] <- 1
+    x_temp[2, 1] <- 1
     row.names(x_temp) <- c("depth","current.parent.node")
 
     x_map <- tibble::tibble(
@@ -133,8 +91,8 @@ tree_filter <- function(nodes_list, m){
     )
 
     for(i in 2:nrow(x_map)){
-      x_temp[2,x_count_depth[i]+1]=node_id[i]
-      x_map$parent_id[i]=x_temp[2,x_count_depth[i]]
+      x_temp[2,x_count_depth[i]+1] <- node_id[i]
+      x_map$parent_id[i] <- x_temp[2,x_count_depth[i]]
     }
 
     A <- matrix(0, nrow = nrow(x_map), ncol = nrow(x_map))
@@ -148,7 +106,7 @@ tree_filter <- function(nodes_list, m){
       dplyr::filter(d == dd) |> 
       dplyr::arrange(node_id) |> 
       dplyr::select(-c(d,dd)) |> 
-      dplyr::mutate(dplyr::across(everything(),as.character))
+      dplyr::mutate(dplyr::across(tidyselect::everything(), as.character))
 
     y$leaf <- cumsum(as.numeric(!y$node_id %in% y$parent_id)) * as.numeric(!y$node_id %in% y$parent_id)
 
@@ -180,18 +138,64 @@ tree_filter <- function(nodes_list, m){
           FILTER = paste0(l, collapse = "&")
         )
     })
-    
+
     active_filter <- active_filter_list |>
       dplyr::bind_rows(.id = "id")
-  
+
   active_filter$id <- as.character(active_filter$id)
 
   names(active_filter)[which(names(active_filter) == 'id')] <- 'ID'
-    
+
   }
-  
+
   return(active_filter)
 }
 
 utils::globalVariables(c('d', 'dd', 'node_name', 'l'))
 
+make_list <- function(x, value = NULL) {
+
+  if(is.null(value)){
+    value <- names(x)
+  }
+
+  idx <- is.na(x[,2])
+  if (ncol(x) > 2 && sum(idx) != nrow(x)){
+
+    listSplit <- split(x[-1], x[1], drop = TRUE)
+    colName <- value[1]
+    value <- value[-1]
+
+    lapply(names(listSplit), function(y){
+      list(
+        name = y,
+        value = colName,
+        children = make_list(listSplit[[y]],value=value)
+        )
+      })
+  } else {
+
+    if(!all(is.na(x[,1]))){
+
+      nms <- x[,1]
+      col_name <- value[1]
+
+      lapply(seq_along(nms), function(y){
+        list(
+          name = nms[y],
+          value = col_name
+        )
+      })
+    }
+  }
+}
+
+renquote <- function(l) {
+
+  if (is.list(l)){
+    lapply(l, renquote)
+  } else {
+    enquote(l)
+  }
+
+}
