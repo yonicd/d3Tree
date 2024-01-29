5ce4f9ebe26e5f9add41d0cb9f3d9439bcf4fc0a
Jonathan Sidi <yoni@pinpointstrategies.io>
2024-01-29 14:41:31 -0500

    fix tree_filter issue

--- Miscellaneous/examples/titanic_shiny/server.R
+++ Miscellaneous/examples/titanic_shiny/server.R
@@ -5,17 +5,20 @@ shinyServer(function(input, output, session) {
   output$Hierarchy <- renderUI({
     Hierarchy=names(m)
     Hierarchy=head(Hierarchy,-1)
-    selectizeInput("Hierarchy","Tree Hierarchy",
+    selectizeInput("Hierarchy", "Tree Hierarchy",
                    choices = Hierarchy,multiple=T,selected = Hierarchy,
-                   options=list(plugins=list('drag_drop','remove_button')))
+                   options=list(plugins = list('drag_drop','remove_button')))
   })
   
   network <- reactiveValues()
   
   observeEvent(input$d3_update,{
-    network$nodes <- unlist(input$d3_update$.nodesData)
-    activeNode<-input$d3_update$.activeNode
-    if(!is.null(activeNode)) network$click <- jsonlite::fromJSON(activeNode)
+    nodesData <- input$d3_update$.nodesData
+    network$nodes <- unlist(nodesData)
+    activeNode <- input$d3_update$.activeNode
+    if(!is.null(activeNode)){
+      network$click <- jsonlite::fromJSON(activeNode)
+    }
   })
   
   observeEvent(network$click,{
@@ -26,13 +29,18 @@ shinyServer(function(input, output, session) {
  
   
   TreeStruct=eventReactive(network$nodes,{
-    df=m
+    df <- m
     if(is.null(network$nodes)){
-      df=m
+      df <- m
     }else{
+      mynodes <- network$nodes
+      x.filter <- tree_filter(network$nodes)
+ 
+      x.filter.tbl <- x.filter |>
+        tibble::as_tibble() |>
+        dplyr::mutate(data = purrr::map(FILTER,function(f){m |> filter(eval(rlang::parse_expr(f))) |> distinct()}))
       
-      x.filter=tree.filter(network$nodes,m)
-      df=ddply(x.filter,.(ID),function(a.x){m%>%filter_(.dots = list(a.x$FILTER))%>%distinct})
+      df <- dplyr::bind_rows(x.filter.tbl$data)
     }
     df
   })
@@ -40,9 +48,9 @@ shinyServer(function(input, output, session) {
   observeEvent(input$Hierarchy,{
     output$d3 <- renderD3tree({
       if(is.null(input$Hierarchy)){
-        p=m
+        p <- m
       }else{
-        p=m%>%select(one_of(c(input$Hierarchy,"NEWCOL")))%>%unique
+        p <- m |> select(one_of(c(input$Hierarchy,"NEWCOL"))) |> unique()
       }
       
       d3tree(data = list(root = df2tree(struct = p,rootname = 'Titanic'), layout = 'collapse'),activeReturn = c('name','value','depth','id'),height = 18)
@@ -51,14 +59,14 @@ shinyServer(function(input, output, session) {
 
   observeEvent(network$nodes,{
     output$results <- renderPrint({
-      str.out=''
-      if(!is.null(network$nodes)) str.out=tree.filter(network$nodes,m)
+      str.out <- ''
+      if(!is.null(network$nodes)) str.out=tree_filter(network$nodes)
       return(str.out)
     })    
   })
 
   output$table <- renderTable(expr = {
-    TreeStruct()%>%select(-NEWCOL)
+    TreeStruct() |> select(-NEWCOL)
   })
   
 })
